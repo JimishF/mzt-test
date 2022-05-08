ca12af38125a7c2a666dc4f143b99e7e9642b3f0
Jimish Fotariya <fotariyajimish@gmail.com>
2022-05-08 13:37:14 +0530

    fix: return type of candidate service listfor

--- app/Http/Services/CandidateService.php
+++ app/Http/Services/CandidateService.php
@@ -14,7 +14,7 @@ use Ramsey\Collection\Collection;
 
 class CandidateService
 {
-    public function listFor($company): Collection
+    public function listFor($company)
     {
         $candidates = Candidate::with('companiesPivot')->get();
         $candidates = $candidates->filter(function ($candidate) use ($company) {
@@ -29,6 +29,7 @@ class CandidateService
 
             return true;
         });
+
         return $candidates->values();
     }
 
